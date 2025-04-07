2f16953c2c6dc6a1ddbd2b0c925785003d402037
Rupayan Ghosh <152713657+Rup-2004@users.noreply.github.com>
2025-04-07 12:41:47 +0530

    SUBNETTING.java

--- SUBNETTING.java
+++ SUBNETTING.java
@@ -96,3 +96,17 @@ public class SUBNETTING {
         
     }
 }
+
+
+//OUTPUT:
+
+//Enter the IP address:
+// 10.10.10.1
+// Address range of subnet1=
+// 10.0.0.0
+// Address range of subnet2=
+// 10.0.0.64
+// Address range of subnet3=
+// 10.0.0.128
+// Address range of subnet4=
+// 10.0.0.192
